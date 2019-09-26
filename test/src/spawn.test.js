c9b7b030a7846bae73014e53d5080b5e0de04700
nmccready <nemtcan@gmail.com>
2019-09-26 13:41:36 -0400

    test for formatter

--- test/src/spawn.test.js
+++ test/src/spawn.test.js
@@ -1,51 +1,57 @@
-var debugFabAPI = require('../../src');
+const debugFabAPI = require('../../src');
 
-var origDebug = debugFabAPI();
+const origDebug = debugFabAPI();
 origDebug.save('root*');
 origDebug.enable(origDebug.load());
 
-var debug = debugFabAPI.spawnable('root', origDebug);
+const debug = debugFabAPI.spawnable('root', origDebug);
 
-describe('spawn', function() {
-  describe('namespaces same level identical', function() {
-    it('2 off', function() {
-      var child1 = debug.spawn('child1');
-      var child2 = debug.spawn('child1');
+describe('spawn', () => {
+  describe('namespaces same level identical', () => {
+    it('2 off', () => {
+      const child1 = debug.spawn('child1');
+      const child2 = debug.spawn('child1');
 
       expect(child1).toEqual(child2);
       expect(child1.namespace).toEqual(child2.namespace);
       expect(child1.namespace).toEqual('root:child1');
     });
 
-    it('grandchildren', function() {
-      var child1 = debug.spawn('child1').spawn('grand');
-      var child2 = debug.spawn('child1').spawn('grand');
+    it('grandchildren', () => {
+      const child1 = debug.spawn('child1').spawn('grand');
+      const child2 = debug.spawn('child1').spawn('grand');
 
       expect(child1).toEqual(child2);
       expect(child1.namespace).toEqual(child2.namespace);
       expect(child1.namespace).toEqual('root:child1:grand');
     });
 
-    it('great', function() {
-      var child1 = debug.spawn('child1').spawn('grand').spawn('great');
-      var child2 = debug.spawn('child1').spawn('grand').spawn('great');
+    it('great', () => {
+      const child1 = debug
+        .spawn('child1')
+        .spawn('grand')
+        .spawn('great');
+      const child2 = debug
+        .spawn('child1')
+        .spawn('grand')
+        .spawn('great');
 
       expect(child1).toEqual(child2);
       expect(child1.namespace).toEqual(child2.namespace);
       expect(child1.namespace).toEqual('root:child1:grand:great');
     });
   });
-  describe('namespaces are unique', function() {
-    it('1 off', function() {
-      var child = debug.spawn('child1');
+  describe('namespaces are unique', () => {
+    it('1 off', () => {
+      const child = debug.spawn('child1');
       expect(child.namespace).not.toEqual(debug.namespace);
       expect(debug.namespace).toEqual('root');
       expect(child.namespace).toEqual('root:child1');
     });
 
-    it('2 off', function() {
-      var child1 = debug.spawn('child1');
-      var child2 = debug.spawn('child2');
+    it('2 off', () => {
+      const child1 = debug.spawn('child1');
+      const child2 = debug.spawn('child2');
 
       expect(child1).not.toEqual(child2);
       expect(child1.namespace).not.toEqual(child2.namespace);
@@ -53,9 +59,9 @@ describe('spawn', function() {
       expect(child2.namespace).toEqual('root:child2');
     });
 
-    it('grandchildren', function() {
-      var child1 = debug.spawn('child1').spawn('grand');
-      var child2 = debug.spawn('child2').spawn('grand');
+    it('grandchildren', () => {
+      const child1 = debug.spawn('child1').spawn('grand');
+      const child2 = debug.spawn('child2').spawn('grand');
 
       expect(child1).not.toEqual(child2); // resolved by noop scope level to be new on each spawn
       expect(child1.namespace).not.toEqual(child2.namespace);
@@ -65,9 +71,9 @@ describe('spawn', function() {
       expect(child2.namespace).toEqual('root:child2:grand');
     });
 
-    it('grandchildren', function() {
-      var child1 = debug.spawn('child1').spawn('grand');
-      var child2 = debug.spawn('child2').spawn('grand1');
+    it('grandchildren', () => {
+      const child1 = debug.spawn('child1').spawn('grand');
+      const child2 = debug.spawn('child2').spawn('grand1');
 
       expect(child1).not.toEqual(child2);
       expect(child1.namespace).not.toEqual(child2.namespace);
@@ -77,9 +83,15 @@ describe('spawn', function() {
       expect(child2.namespace).toEqual('root:child2:grand1');
     });
 
-    it('great grandchildren', function() {
-      var child1 = debug.spawn('child1').spawn('grand').spawn('great');
-      var child2 = debug.spawn('child2').spawn('grand').spawn('great');;
+    it('great grandchildren', () => {
+      const child1 = debug
+        .spawn('child1')
+        .spawn('grand')
+        .spawn('great');
+      const child2 = debug
+        .spawn('child2')
+        .spawn('grand')
+        .spawn('great');
 
       expect(child1).not.toEqual(child2);
       expect(child1.namespace).not.toEqual(child2.namespace);
