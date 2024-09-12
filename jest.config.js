2a50d49b8b08b2b9f89868a084e97c000b5bcabf
nmccready <nemtcan@gmail.com>
2024-09-12 18:16:32 -0400

    chore: ts-jest save off

--- jest.config.js
+++ jest.config.js
@@ -1,32 +1,8 @@
-const toNotIgnore = {
-  modules: [].join('|'),
-};
-
-const toExport = {
-  roots: ['<rootDir>'],
-  // moduleFileExtensions: ['js', 'css', 'ts'],
-  transformIgnorePatterns: ['node_modules', '/<rootDir>/lib', '/<rootDir>/(?!src)'],
-  moduleNameMapper: {
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      '<rootDir>/node_modules/@znemz/react-extras-jest/lib/assetsTransformer.js',
-    '\\.(css|less|pcss)(!js)$':
-      '<rootDir>/node_modules/@znemz/react-extras-jest/lib/assetsTransformer.js',
+/** @type {import('ts-jest').JestConfigWithTsJest} * */
+module.exports = {
+  testEnvironment: 'node',
+  transform: {
+    '^.+.tsx?$': ['ts-jest', {}],
   },
-  setupFilesAfterEnv: ['<rootDir>/node_modules/@znemz/react-extras-jest/lib/setup.js'],
-  verbose: true,
-  collectCoverageFrom: [
-    'src/**/*.{ts,js}',
-    '!<rootDir>/node_modules/',
-    '!<rootDir>/lib/',
-    '!<rootDir>/umd/',
-    '!<rootDir>/config/',
-    '!<rootDir>/test/',
-  ],
-  testRegex: '(/test/.*(test|spec))\\.[jt]sx?$',
+  testMatch: ['**/?(*.)+(spec|test).[tj]s?(x)'],
 };
-
-if (toNotIgnore.modules.length) {
-  toExport.transformIgnorePatterns.push(`/node_modules/(?!(${toNotIgnore.modules}))`);
-}
-
-module.exports = toExport;
